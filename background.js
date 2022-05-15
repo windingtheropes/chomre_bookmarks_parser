85cb38b6dc9e8a7f37d2589a4ca68e06011bde02
windingtheropes <windingtheropes@gmail.com>
2022-05-15 15:04:00 -0400

    make working parser, but the project is cancelled as filesystem service worker access is not allowed

--- background.js
+++ background.js
@@ -1,61 +1,84 @@
+// Sync when first installed and when bookmarks are updated
+// Also clear the tree on every update
+
+var tree = []
+
 chrome.runtime.onInstalled.addListener(() => {
-    readRoot("0");
+    syncTree()
 })
 
-const tree = []
+chrome.bookmarks.onChanged.addListener(() => {
+    syncTree()
+})
 
-const readFolder = (queryFolderId, prevFolderId, qArr) => {
+function syncTree()
+{
+    tree = []
+    readRoot()
+}
+
+function findInTree(path)
+{
+    var folder
+    path.forEach(q => {
+        if(!folder) folder = tree.find(f => f.id === q)
+        else folder = folder.content.find(f => f.id === q)
+        if(!folder) return false
+    })
+    return folder
+}
+
+function readFolder(queryFolderId, path) // query is the folder to query, path leads to the query's parent 
+{
     console.log(tree)
-    chrome.bookmarks.getChildren(queryFolderId).then(async parents => {
+    chrome.bookmarks.getChildren(queryFolderId).then(async children => { // gtes an array of children
+        // everything here belongs to queryFolder
+        const { cBookmarks, cFolders } = await readChildren(children)
+        
+        const parentFolder = findInTree(path)
+        const folder = parentFolder.content.find(f => f.id === queryFolderId)
 
-        const { bookmarks, folders } = await readChildren(parents)
+        cFolders.forEach(f => {
+            folder.content.push({ title: f.title, id: f.id, parentId: queryFolderId, type: "folder" , content: [] })
+            path.push(queryFolderId)
+            readFolder(f.id, path) // this folder belongs to queryFolder
+        })
 
-        for (const bookmark of bookmarks) {
-            if (tree.find(f => f.id === prevFolderId).content
-                .find(f => f.id === queryFolderId).content.find(p => p.id === bookmark.id)) continue
-
-            tree.find(f => f.id === prevFolderId).content
-                .find(f => f.id === queryFolderId).content
-                .push({ type: "bookmark", id: bookmark.id, title: bookmark.title, url: bookmark.url })
-        }
-
-        for (const folder of folders) {
-            if (tree.find(f => f.id === prevFolderId).content
-            .find(f => f.id === queryFolderId).content.find(p => p.id === folder.id)) continue
-
-            tree.find(f => f.id === prevFolderId).content
-                .find(f => f.id === queryFolderId).content
-                .push({ type: "folder", id: folder.id, title: folder.title, content: [] })
-            readFolder(folder.id, queryFolderId, qArr.push(queryFolderId))
-        }
-
+        cBookmarks.forEach(b => {
+            folder.content.push({ title: b.title, id: b.id, parentId: queryFolderId, type: "bookmark" , url: b.url })
+        })
+    
     })
 }
 
-const readRoot = async folderId => {
-    chrome.bookmarks.getChildren(folderId).then(parents => {
-        parents.forEach(p => {
-            if (!tree.find(f => f.id === p.id)) { tree.push({ id: p.id, title: p.title, content: [] }) }
+function readRoot(){
+    chrome.bookmarks.getChildren('0').then(roots => { // this reads the folders at root, parented byt 0, like the bookmarks bar, other and mobile
+        // everything in here belongs to root (0)
+        roots.forEach(p => {
+            if (!tree.find(f => f.id === p.id)) { tree.push({ title: p.title, id: p.id, parentId: 0, type: "folder" , content: [] }) }
             chrome.bookmarks.getChildren(p.id).then(async children => {
-                const { bookmarks, folders } = await readChildren(children)
-                bookmarks.forEach(b => {
-                    tree.find(f => f.id === p.id).content.push({ type: "bookmark", id: b.id, title: b.title, url: b.url })
-                    // console.log(b.title)
+                // everything in here belongs to p
+
+                const { cBookmarks, cFolders } = await readChildren(children)
+
+
+                cFolders.forEach(f => {
+                    tree.find(f => f.id === p.id).content.push({ title: f.title, id: f.id, parentId: p.id, type: "folder" , content: [] }) // need to get the path to this folder
+                    readFolder(f.id, ['0', p.id]) // the path to the folder is 0, f.id. f.id is to use the api. path is for tree.
                 })
-                folders.forEach(f => {
-                    tree.find(f => f.id === p.id).content.push({ type: "folder", id: f.id, title: f.title, content: [] })
-                    // console.log(f.title)
-                    readFolder(f.id, p.id, [p.id])
+
+                cBookmarks.forEach(b => {
+                    tree.find(f => f.id === p.id).content.push({ title: b.title, id: b.id, parentId: p.id, type: "bookmark" , url: b.url })
                 })
+    
             })
         })
     })
 }
 
-
 const readChildren = async (children) => {
-    const bookmarks = await children.filter(c => c.url)
-    const folders = await children.filter(c => !c.url)
+    const cBookmarks = await children.filter(c => c.url)
+    const cFolders = await children.filter(c => !c.url)
 
-    return { bookmarks, folders }
+    return { cBookmarks, cFolders }
 }
